328b01b9a8824f3abd7333299116498e00f507d7
TommyChinchay <83140197+TommyChinchay@users.noreply.github.com>
2023-11-07 17:20:14 -0300

    Update AppLibrosTest.java

--- src/test/java/AppLibrosTest.java
+++ src/test/java/AppLibrosTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.api.Test;
 
 public class AppLibrosTest {
 
-
+    @Test
+    public void testAlgoQueDeberiaEstarBien() {
+        Assertions.assertEquals(1, 1);
+    }
 
 
 }
